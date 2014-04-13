6ecca823519a9edc0f610169187546be00aad461
Johannes Wilm <johannes@wilm.no>
2014-04-13 20:51:19 +0200

    use overflow testing and columns for nicer cutoffs

--- overflow-cut.js
+++ overflow-cut.js
@@ -1,6 +1,6 @@
 function flowElement(node, container) {
     var overflow = document.createDocumentFragment();
-    
+
     while (node.firstChild) {
         overflow.appendChild(node.firstChild);
     }
@@ -8,16 +8,16 @@ function flowElement(node, container) {
     function checkForOverflow(el) {
         var isOverflowing,
             curOverflow = el.style.overflow;
-        if ( !curOverflow || curOverflow === "visible" ) {
+        if (!curOverflow || curOverflow === "visible") {
             el.style.overflow = "hidden";
         }
-        isOverflowing = el.clientHeight < (el.scrollHeight-10);
+        isOverflowing = el.clientHeight < (el.scrollHeight - 10);
         el.style.overflow = curOverflow;
         return isOverflowing;
     };
-    
-    
-    function createPage  () {
+
+
+    function createPage() {
         var pageNode = document.createElement('div');
         pageNode.classList.add('page');
         var contentArea = document.createElement('div');
@@ -26,31 +26,32 @@ function flowElement(node, container) {
         container.appendChild(pageNode);
         return contentArea;
     }
-    
+
     var splitter = 0;
-    
-    function fillPage (node) {
-        var lastPage = createPage(), overflow = document.createDocumentFragment();
-    
+
+    function fillPage(node) {
+        var lastPage = createPage(),
+            overflow = document.createDocumentFragment();
+
         lastPage.appendChild(node);
-    
-        while ( checkForOverflow(lastPage) ) {
+
+        while (checkForOverflow(lastPage)) {
             overflow.insertBefore(lastPage.lastChild, overflow.firstChild);
         }
-        if (overflow.firstChild) {// || overflow.firstChild.nodeType === 3) {
+        if (overflow.firstChild) { // || overflow.firstChild.nodeType === 3) {
             var splitNode = overflow.firstChild;
-        
+
             lastPage.appendChild(splitNode);
-        
+
             splitNode.classList.add('split');
             splitNode.setAttribute('data-split', splitter++);
             var newNode = splitNode.cloneNode();
             overflow.insertBefore(newNode, overflow.firstChild);
-            while ( checkForOverflow(lastPage) ) {
+            while (checkForOverflow(lastPage)) {
                 newNode.insertBefore(splitNode.lastChild, newNode.firstChild);
             }
-            
-            if (splitNode.childNodes.length===0) {
+
+            if (splitNode.childNodes.length === 0) {
                 lastPage.removeChild(splitNode);
                 newNode.classList.remove('split');
                 newNode.removeAttribute('data-split');
@@ -58,9 +59,9 @@ function flowElement(node, container) {
             } else {
                 console.log('something');
             }
-        }    
-            
-        if (overflow.childNodes.length>0) {
+        }
+
+        if (overflow.childNodes.length > 0) {
             fillPage(overflow);
         }
     }
--- range-cut.js
+++ range-cut.js
@@ -1,24 +1,24 @@
 function flowElement(node, container) {
     var overflow = document.createDocumentFragment();
-    
+
     while (node.firstChild) {
         overflow.appendChild(node.firstChild);
     }
 
-    function caretRange(x,y) {
+    function caretRange(x, y) {
         if (document.caretPositionFromPoint) {
             // Firefox
-            var position = document.caretPositionFromPoint(x,y),
+            var position = document.caretPositionFromPoint(x, y),
                 range = document.createRange();
             range.setStart(position.offsetNode, position.offset);
             return range;
         } else {
             // Webkit + Chrome
-            return document.caretRangeFromPoint(x,y);
+            return document.caretRangeFromPoint(x, y);
         }
     };
-    
-    function createPage  () {
+
+    function createPage() {
         var pageNode = document.createElement('div');
         pageNode.classList.add('page');
         var contentArea = document.createElement('div');
@@ -27,37 +27,52 @@ function flowElement(node, container) {
         container.appendChild(pageNode);
         return contentArea;
     }
-    
+
     var splitter = 0;
-    
-    function fillPage (node) {
 
-        var lastPage = createPage(), coordinates, bottomLeftX, bottomLeftY;
 
-        
-            lastPage.appendChild(node);
-            lastPage.scrollIntoView(true);
-            coordinates = lastPage.getBoundingClientRect(); 
-            bottomLeftX = coordinates.left; 
-            bottomLeftY = coordinates.bottom;          
-            
-            
-            range = caretRange(bottomLeftX+1, bottomLeftY-1); 
-            
-            if (range) {
-                range.setEndAfter(lastPage.lastChild);
-                overflow = range.extractContents();
-            
-                if ((overflow.firstChild.nodeName==='P'||overflow.firstChild.nodeName==='DIV') && overflow.firstChild.innerHTML.length===0) {
-                    overflow.removeChild(overflow.firstChild);
-                }
-                if (lastPage.innerHTML==='<p></p>'||lastPage.innerHTML==='<div></div>') {
-                    lastPage.removeChild(lastPage.firstChild);
-                    lastPage.appendChild(overflow);
-                } else if (overflow.firstChild && lastPage.firstChild) {
-                    fillPage(overflow);
-                }
+    var columnWidth;
+
+    if (document.body.style.webkitColumnWidth) {
+        // Webkit + Chrome
+        columnWidth = 'webkitColumnWidth';
+    } else {
+        // Firefox
+        columnWidth = 'mozColumnWidth';
+    }
+
+    function fillPage(node) {
+
+        var lastPage = createPage(),
+            coordinates, bottomLeftX, bottomLeftY;
+
+
+        lastPage.appendChild(node);
+        lastPage.scrollIntoView(true);
+
+        if (lastPage.scrollHeight > lastPage.clientHeight) {
+            lastPage.style[columnWidth] = lastPage.clientWidth + 'px';
+
+            coordinates = lastPage.getBoundingClientRect();
+            bottomLeftX = coordinates.left;
+            bottomLeftY = coordinates.bottom;
+            range = caretRange(bottomLeftX + 1, bottomLeftY - 1);
+
+
+            range.setEndAfter(lastPage.lastChild);
+            overflow = range.extractContents();
+            lastPage.style[columnWidth] = 'auto';
+            if ((overflow.firstChild.nodeName === 'P' || overflow.firstChild.nodeName === 'DIV') && overflow.firstChild.innerHTML.length === 0) {
+                overflow.removeChild(overflow.firstChild);
             }
+            if (lastPage.innerHTML === '<p></p>' || lastPage.innerHTML === '<div></div>') {
+                lastPage.removeChild(lastPage.firstChild);
+                lastPage.appendChild(overflow);
+            } else if (overflow.firstChild && lastPage.firstChild) {
+                fillPage(overflow);
+            }
+
+        }
     }
     fillPage(overflow);
 }
