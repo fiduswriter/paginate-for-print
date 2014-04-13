16dc3ca203d2f6660cdf78d99f35d08c98798189
Johannes Wilm <johannes@wilm.no>
2014-04-14 00:11:13 +0200

    footnote support and name changes to work with pagination.js

--- range-cut.js
+++ range-cut.js
@@ -16,16 +16,49 @@ function flowElement(node, container) {
             // Webkit + Chrome
             return document.caretRangeFromPoint(x, y);
         }
-    };
+    }
 
+    
+    function cutToFit(contents) {
+
+            contents.style.height = (contents.parentElement.clientHeight-contents.nextSibling.clientHeight)+'px';
+
+            contents.style[columnWidth] = contents.clientWidth + 'px';
+
+            var coordinates = contents.getBoundingClientRect(),
+            bottomLeftX = coordinates.left,
+            bottomLeftY = coordinates.bottom,
+            range = caretRange(bottomLeftX + 1, bottomLeftY - 1);
+
+
+            range.setEndAfter(contents.lastChild);
+            overflow = range.extractContents();
+            
+
+            return overflow;
+    }
+    
     function createPage() {
-        var pageNode = document.createElement('div');
-        pageNode.classList.add('page');
-        var contentArea = document.createElement('div');
-        contentArea.classList.add('content');
-        pageNode.appendChild(contentArea);
-        container.appendChild(pageNode);
-        return contentArea;
+        var page = document.createElement('div');
+        page.classList.add('pagination-page');
+        contentsContainer = document.createElement('div');
+        contentsContainer.classList.add('pagination-contents-container');
+        mainContentsContainer = document.createElement('div');
+        mainContentsContainer.classList.add('pagination-main-contents-container');
+        
+        contents = document.createElement('div');
+        contents.classList.add('pagination-contents');
+        
+        footnotes = document.createElement('div');
+        footnotes.classList.add('pagination-footnotes');
+        footnotes.innerHTML="<p></p>";
+        
+        mainContentsContainer.appendChild(contents);
+        mainContentsContainer.appendChild(footnotes);
+        
+        page.appendChild(mainContentsContainer);
+        container.appendChild(page);
+        return contents;
     }
 
     var splitter = 0;
@@ -43,25 +76,45 @@ function flowElement(node, container) {
 
     function fillPage(node) {
 
-        var lastPage = createPage(),
-            coordinates, bottomLeftX, bottomLeftY;
+        var lastPage = createPage(), clonedNode = node.cloneNode(true), footnotes, footnotesLength, clonedFootnote, i, oldFn, fnHeightTotal;
 
 
         lastPage.appendChild(node);
         lastPage.scrollIntoView(true);
 
         if (lastPage.scrollHeight > lastPage.clientHeight) {
-            lastPage.style[columnWidth] = lastPage.clientWidth + 'px';
+            
+            overflow = cutToFit(lastPage);
+            
+            
+            footnotes = lastPage.querySelectorAll('.pagination-footnote');
+            footnotesLength = footnotes.length;
+            if (footnotesLength > 0) {
+                for (i=0;i<footnotesLength;i++) {
+                    clonedFootnote = footnotes[i].cloneNode(true);
+                    lastPage.nextSibling.appendChild(clonedFootnote);
+                }
+                
+                while (lastPage.firstChild) {
+                    lastPage.removeChild(lastPage.firstChild);
+                }
+                
+                
+                lastPage.appendChild(clonedNode);
+                lastPage.scrollIntoView(true);
+                
+                overflow = cutToFit(lastPage);
+                
+                for (i=lastPage.querySelectorAll('.pagination-footnote').length;i<footnotesLength;i++) {
+                    oldFn = lastPage.nextSibling.children[i];
+                    
+                    while(oldFn.firstChild) {
+                        oldFn.removeChild(oldFn.firstChild);
+                    }
+                } 
+            }
+            
 
-            coordinates = lastPage.getBoundingClientRect();
-            bottomLeftX = coordinates.left;
-            bottomLeftY = coordinates.bottom;
-            range = caretRange(bottomLeftX + 1, bottomLeftY - 1);
-
-
-            range.setEndAfter(lastPage.lastChild);
-            overflow = range.extractContents();
-            lastPage.style[columnWidth] = 'auto';
             if ((overflow.firstChild.nodeName === 'P' || overflow.firstChild.nodeName === 'DIV') && overflow.firstChild.innerHTML.length === 0) {
                 overflow.removeChild(overflow.firstChild);
             }
